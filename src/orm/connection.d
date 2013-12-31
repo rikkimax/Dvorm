c240727d20c89449df5fb5f18a074626eef14167
rikki cattermole <pbdadmin@gmail.com>
2013-12-31 18:31:35 +1300

    Adding support for vibe serialization with DbConnection. Possibly for usage as a configuration means.

--- src/orm/connection.d
+++ src/orm/connection.d
@@ -1,5 +1,9 @@
 module dvorm.connection;
 
+static if (__traits(compiles, {import vibe.data.serialization;})) {
+	import vibe.data.serialization;
+}
+
 string connection() {
 	string ret;
 	ret ~=
@@ -42,6 +46,11 @@ static {
 
 struct DbConnection {
 	DbType type;
+
+	static if (__traits(compiles, {import vibe.data.serialization;})) {
+		// just so we can use this for configuration :3
+	@optional:
+	}
 
 	string host;
 	ushort port;
