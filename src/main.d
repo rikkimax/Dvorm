f53059fc719c9b4ad544ac6e0d3246ac5d1170ee
rikki cattermole <pbdadmin@gmail.com>
2014-01-09 20:38:04 +1300

    Hmm I thought it automatically tested memory database provider already?

--- src/main.d
+++ src/main.d
@@ -5,5 +5,6 @@ import dvorm.unittests;
 void main() {
 	version(unittest) {
 		unittest1(DbConnection(DbType.Mongo, "192.168.100.10", ushort.init, null, null, "aa_test"));
+		unittest1(DbConnection(DbType.Memory));
 	}
 }
