cbf3af20f0150f6f29df683265fc373533b64fff
rikki cattermole <pbdadmin@gmail.com>
2014-02-18 15:27:31 +1300

    Huh woops forgot that it already had the receive type check, just slightly in wrong place.

--- src/email/providers/email.d
+++ src/email/providers/email.d
@@ -113,158 +113,152 @@ void*[] getData(ObjectBuilder builder, bool delegate(EmailMessage message) handl
 			
 			void*[] ret;
 			
-			switch(receiveType) {
-				case ReceiveClientType.Pop3:
-					conn.write("USER " ~ receiveConfig.user ~ "\r\n");
-					
-					got = cast(string)conn.readLine();
-					debug {
-						append("out.txt", "user: " ~ got ~ "\n");
-					}
-					
-					conn.write("PASS " ~ receiveConfig.password ~ "\r\n");
-					
-					got = cast(string)conn.readLine();
-					debug {
-						append("out.txt", "pass: " ~ got ~ "\n");
-					}
-					
-					conn.write("LIST \r\n");
-					
-					got = cast(string)conn.readLine();
-					debug {
-						append("out.txt", "list: " ~ got ~ "\n");
-					}
-					
-					string[] ids;
-					while((got = cast(string)conn.readLine()) != ".") {
-						string[] splitted = got.split(" ");
-						if (splitted.length == 2) {
-							ids ~= splitted[0];
-						}
-					}
-					
-					foreach(id; ids) {					
-						string from;
-						string target;
-						string date;
-						string contentType;
-						string subject;
-						string message;
-						
-						string boundry;
-						bool hitEndOfHeaders = false;
-						bool useBoundry = false;
-						bool hitBoundry = false;
-						
-						string[] splitted;
-						
-						conn.write("RETR " ~ id ~ "\r\n");
-						while((got = cast(string)conn.readLine()) != ".") {
-							
-							if (got.length > "\r\n.".length) {
-								if (got[0 .. "\r\n.".length] == "\r\n.") {
-									got = got["\r\n.".length - 1 .. $];
-								}
-							}
-							
-							append("out.txt", "retr " ~ id ~ ": " ~ got ~ "\n");
-							
-							splitted = got.split(" ");
-							if (splitted.length >= 2) {
-								switch(splitted[0].toLower()) {
-									case "from:":
-										from = got["from: ".length .. $];
-										break;
-									case "to:":
-										target = splitted[1];
-										break;
-									case "subject:":
-										subject = got["subject: ".length .. $];
-										break;
-									case "date:":
-										date = got["date: ".length .. $];
-										break;
-									case "content-type:":
-										contentType = splitted[1];
-										break;
-									default:
-										break;
-								}
-							}
-							
-							if (contentType.length > "multipart/mixed; boundary=".length) {
-								if (contentType[0 .. "multipart/mixed; boundary=".length] == "multipart/mixed; boundary=\"") {
-									boundry = contentType["multipart/mixed; boundary=".length + 1 .. $-1];
-								} else if (contentType[0 .. "multipart/mixed; boundary=".length] == "multipart/mixed; boundary=") {
-									boundry = contentType["multipart/mixed; boundary=".length .. $];
-								}
-							}
-							
-							if (hitBoundry) {
-								message ~= got ~ "\n";
-							}
-							
-							if (hitEndOfHeaders) {
-								if (useBoundry) {
-									if (!hitBoundry && got == "--" ~ boundry) {
-										hitBoundry = true;
-									} else if (hitBoundry && got == "--" ~ boundry ~ "--") {
-										hitBoundry = false;
-									}
-								} else {
-									hitBoundry = true;
-								}
-								
-								if (boundry != "") {
-									useBoundry = true;
-								}
-							}
-							
-							if (got == "")
-								hitEndOfHeaders = true;
-						}
-						
-						string[string] build = ["target" : target, "contentType": contentType, "subject": subject, "message": message];
-						splitted = from.split(" ");
-						if (splitted.length == 1) {
-							string[] splitted2 = from.split("@");
-							if (splitted2.length == 2) {
-								build["from_user"] = splitted2[0];
-								build["from_domain"] = splitted2[1];
-							}
-						} else if (splitted[$-1][0] == '<' && splitted[$-1][$-1] == '>') {
-							string text = splitted[$-1][1 .. $-1];
-							string[] splitted2 = splitted[$-1].split("@");
-							build["from_user"] = splitted2[0][1 .. $];
-							build["from_domain"] = splitted2[1][0 .. $-1];
-							build["from_name"] = splitted[0 .. $-1].join(" ");
-						}
-						
-						// TODO convert date to UTC+0 unix time
-						
-						EmailMessage* tempObj = cast(EmailMessage*)builder(build);
-						
-						if (handler !is null) {
-							if (handler(*tempObj))
-								ret ~= tempObj;
-						} else
-							ret ~= tempObj;
-						
-						debug {
-							append("out.txt", "parsed " ~ id ~ ": " ~ from ~ "\n");
-							append("out.txt", "parsed " ~ id ~ ": " ~ target ~ "\n");
-							append("out.txt", "parsed " ~ id ~ ": " ~ date ~ "\n");
-							append("out.txt", "parsed " ~ id ~ ": " ~ contentType ~ "\n");
-							append("out.txt", "parsed " ~ id ~ ": " ~ subject ~ "\n");
-							append("out.txt", "parsed " ~ id ~ ": " ~ message ~ "\n");
-						}
-					}
-					
-					break;
-				default:
-					break;
+			conn.write("USER " ~ receiveConfig.user ~ "\r\n");
+			
+			got = cast(string)conn.readLine();
+			debug {
+				append("out.txt", "user: " ~ got ~ "\n");
 			}
+			
+			conn.write("PASS " ~ receiveConfig.password ~ "\r\n");
+			
+			got = cast(string)conn.readLine();
+			debug {
+				append("out.txt", "pass: " ~ got ~ "\n");
+			}
+			
+			conn.write("LIST \r\n");
+			
+			got = cast(string)conn.readLine();
+			debug {
+				append("out.txt", "list: " ~ got ~ "\n");
+			}
+			
+			string[] ids;
+			while((got = cast(string)conn.readLine()) != ".") {
+				string[] splitted = got.split(" ");
+				if (splitted.length == 2) {
+					ids ~= splitted[0];
+				}
+			}
+			
+			foreach(id; ids) {					
+				string from;
+				string target;
+				string date;
+				string contentType;
+				string subject;
+				string message;
+				
+				string boundry;
+				bool hitEndOfHeaders = false;
+				bool useBoundry = false;
+				bool hitBoundry = false;
+				
+				string[] splitted;
+				
+				conn.write("RETR " ~ id ~ "\r\n");
+				while((got = cast(string)conn.readLine()) != ".") {
+					
+					if (got.length > "\r\n.".length) {
+						if (got[0 .. "\r\n.".length] == "\r\n.") {
+							got = got["\r\n.".length - 1 .. $];
+						}
+					}
+					
+					append("out.txt", "retr " ~ id ~ ": " ~ got ~ "\n");
+					
+					splitted = got.split(" ");
+					if (splitted.length >= 2) {
+						switch(splitted[0].toLower()) {
+							case "from:":
+								from = got["from: ".length .. $];
+								break;
+							case "to:":
+								target = splitted[1];
+								break;
+							case "subject:":
+								subject = got["subject: ".length .. $];
+								break;
+							case "date:":
+								date = got["date: ".length .. $];
+								break;
+							case "content-type:":
+								contentType = splitted[1];
+								break;
+							default:
+								break;
+						}
+					}
+					
+					if (contentType.length > "multipart/mixed; boundary=".length) {
+						if (contentType[0 .. "multipart/mixed; boundary=".length] == "multipart/mixed; boundary=\"") {
+							boundry = contentType["multipart/mixed; boundary=".length + 1 .. $-1];
+						} else if (contentType[0 .. "multipart/mixed; boundary=".length] == "multipart/mixed; boundary=") {
+							boundry = contentType["multipart/mixed; boundary=".length .. $];
+						}
+					}
+					
+					if (hitBoundry) {
+						message ~= got ~ "\n";
+					}
+					
+					if (hitEndOfHeaders) {
+						if (useBoundry) {
+							if (!hitBoundry && got == "--" ~ boundry) {
+								hitBoundry = true;
+							} else if (hitBoundry && got == "--" ~ boundry ~ "--") {
+								hitBoundry = false;
+							}
+						} else {
+							hitBoundry = true;
+						}
+						
+						if (boundry != "") {
+							useBoundry = true;
+						}
+					}
+					
+					if (got == "")
+						hitEndOfHeaders = true;
+				}
+				
+				string[string] build = ["target" : target, "contentType": contentType, "subject": subject, "message": message];
+				splitted = from.split(" ");
+				if (splitted.length == 1) {
+					string[] splitted2 = from.split("@");
+					if (splitted2.length == 2) {
+						build["from_user"] = splitted2[0];
+						build["from_domain"] = splitted2[1];
+					}
+				} else if (splitted[$-1][0] == '<' && splitted[$-1][$-1] == '>') {
+					string text = splitted[$-1][1 .. $-1];
+					string[] splitted2 = splitted[$-1].split("@");
+					build["from_user"] = splitted2[0][1 .. $];
+					build["from_domain"] = splitted2[1][0 .. $-1];
+					build["from_name"] = splitted[0 .. $-1].join(" ");
+				}
+				
+				// TODO convert date to UTC+0 unix time
+				
+				EmailMessage* tempObj = cast(EmailMessage*)builder(build);
+				
+				if (handler !is null) {
+					if (handler(*tempObj))
+						ret ~= tempObj;
+				} else
+					ret ~= tempObj;
+				
+				debug {
+					append("out.txt", "parsed " ~ id ~ ": " ~ from ~ "\n");
+					append("out.txt", "parsed " ~ id ~ ": " ~ target ~ "\n");
+					append("out.txt", "parsed " ~ id ~ ": " ~ date ~ "\n");
+					append("out.txt", "parsed " ~ id ~ ": " ~ contentType ~ "\n");
+					append("out.txt", "parsed " ~ id ~ ": " ~ subject ~ "\n");
+					append("out.txt", "parsed " ~ id ~ ": " ~ message ~ "\n");
+				}
+			}
+			
 			return ret;
 		default:
 			return null;
